f0ffa45106cde1cf695986b6e21fbfcfd3af7f5a
Animesh Kumar <animesh@uber.com>
2019-05-30 13:44:49 -0700

    Added melpa-stable package

--- settings/packages.el
+++ settings/packages.el
@@ -1,4 +1,8 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
 (package-initialize)
 
